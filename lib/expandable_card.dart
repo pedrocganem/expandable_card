c8b3161de0236088b23c4607b82f138ecf4a7fab
Pedro Ganem <67203476+pedrocganem@users.noreply.github.com>
2020-10-23 15:46:50 -0300

    initial commit

---
+++ lib/expandable_card.dart
@@ -0,0 +1,18 @@
+library expandable_card;
+
+import 'package:flutter/material.dart';
+
+class ExpandableCard extends StatefulWidget {
+  
+  @override
+  _ExpandableCardState createState() => _ExpandableCardState();
+}
+
+class _ExpandableCardState extends State<ExpandableCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
